c89eb96be13a5b66a9888964e8ff9ee0a1cc4f26
unknown <aviralkakkar3@gmil.com>
2021-02-25 16:10:51 +0530

    twilio commit

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -250,7 +250,7 @@ define([
         var errorSlds = '<div class="slds-notify slds-notify_alert slds-alert_error" role="alert"><span class="slds-assistive-text">error</span><span class="slds-icon_container slds-icon-utility-error slds-m-right_x-small" title="Description of icon when needed"><svg class="slds-icon slds-icon_x-small" aria-hidden="true"><use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#error"></use></svg></span><h2>Please fill Account SID and Auth Token <a href="#">More Information</a></h2><div class="slds-notify__close"><button class="slds-button slds-button_icon slds-button_icon-small slds-button_icon-inverse" title="Close"><svg class="slds-button__icon" aria-hidden="true"><use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#close"></use></svg><span class="slds-assistive-text">Close</span></button></div></div>';
         if((currentStep.key) === 'step1')
         {
-            console.log( "Account SID KE ANDAR HA" ); 
+            
           var accountSid = $('#accountSID').val();
           var authToken = $('#authToken').val();
               
@@ -259,20 +259,12 @@ define([
                 document.getElementById("error").innerHTML= errorSlds;
                 connection.trigger('prevStep');
               }
-              else if (!authToken)
-              {
-                document.getElementById("authTokenError").innerHTML= "Please Enter Auth Token";
-                connection.trigger('prevStep');
-              }
               else
               {
-                document.getElementById("accountSIDError").innerHTML= "";
-                document.getElementById("authTokenError").innerHTML= "";
+                document.getElementById("error").innerHTML= "";
                 connection.trigger('nextStep');
               }
 
-         
-          
         }
         
         else if (
