4b76558abdcc3334991c18c26f5a1ca857c61119
unknown <aviralkakkar3@gmil.com>
2021-03-03 18:10:45 +0530

    commit

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -105,7 +105,7 @@ define([
                 {		
                     console.log("Transactional message -- . ");
                     $('#template').attr('disabled',false);
-                    $("#myBtn").remove();  	
+                //    $("#myBtn").remove();  	
                 }
             
             var whatsappsms = $("#WhatsApp").is(":checked");  
